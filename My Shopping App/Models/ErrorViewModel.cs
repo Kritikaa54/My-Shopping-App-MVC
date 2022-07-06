49e709ca3d95ee73b30ef90a9120320003aef59b
003U60744 <003U60744@IBM-PF3PBYVS>
2022-07-06 20:42:02 +0530

    Add project files.

---
+++ My Shopping App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace My_Shopping_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ My Shopping App/Models/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace My_Shopping_App.Models
+{
+    public class LoginViewModel
+    {
+      public string Email { get; set; }
+      public string Password { get; set; }
+    }
+}
